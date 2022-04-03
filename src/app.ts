06811d3c32f537843d6580cbcf49b415fa8c0626
wanghaisheng <edwin.uestc@gmail.com>
2022-04-04 06:39:27 +0800

    separate yml

--- src/app.ts
+++ src/app.ts
@@ -739,47 +739,47 @@ async function upwork() {
       // cron.schedule("* * * * *", function () {
       //   // API call goes here
       //   console.log("running a task every minute");
-      const optionstop500 = {
-        hostname: 'localhost',
-        port: 8083,
-        path: '/top500',
-        // path: '/merchantgenius',
-        method: 'GET'
-      }
-      http.get(optionstop500, function (error: any, response: { statusCode: number; }, body: any) {
-        if (!error && response.statusCode == 200) {
-          console.log(body) // Print the google web page.
-        }
-      })
+      // const optionstop500 = {
+      //   hostname: 'localhost',
+      //   port: 8083,
+      //   path: '/top500',
+      //   // path: '/merchantgenius',
+      //   method: 'GET'
+      // }
+      // http.get(optionstop500, function (error: any, response: { statusCode: number; }, body: any) {
+      //   if (!error && response.statusCode == 200) {
+      //     console.log(body) // Print the google web page.
+      //   }
+      // })
 
 
 
-      const optionsmerchantgenius = {
-        hostname: 'localhost',
-        port: 8083,
-        // path: '/top500',
-        path: '/merchantgenius',
-        method: 'GET'
-      }
-      http.get(optionsmerchantgenius, function (error: any, response: { statusCode: number; }, body: any) {
-        if (!error && response.statusCode == 200) {
-          console.log(body) // Print the google web page.
-        }
-      })
+      // const optionsmerchantgenius = {
+      //   hostname: 'localhost',
+      //   port: 8083,
+      //   // path: '/top500',
+      //   path: '/merchantgenius',
+      //   method: 'GET'
+      // }
+      // http.get(optionsmerchantgenius, function (error: any, response: { statusCode: number; }, body: any) {
+      //   if (!error && response.statusCode == 200) {
+      //     console.log(body) // Print the google web page.
+      //   }
+      // })
 
 
 
-      const optionsupwork = {
-        hostname: 'localhost',
-        port: 8083,
-        path: '/upwork',
-        method: 'GET'
-      }
-      http.get(optionsupwork, function (error: any, response: { statusCode: number; }, body: any) {
-        if (!error && response.statusCode == 200) {
-          console.log(body) // Print the google web page.
-        }
-      })      
+      // const optionsupwork = {
+      //   hostname: 'localhost',
+      //   port: 8083,
+      //   path: '/upwork',
+      //   method: 'GET'
+      // }
+      // http.get(optionsupwork, function (error: any, response: { statusCode: number; }, body: any) {
+      //   if (!error && response.statusCode == 200) {
+      //     console.log(body) // Print the google web page.
+      //   }
+      // })      
 
 
 
